4338ef7c3ba59480defd6b2ef6af089c596f26de
donellgill <donell.gill@outlook.com>
2021-04-09 19:34:56 -0400

    manipulated DOM elements

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
 // Write your code here!
+
+const main = document.querySelector('#main');
+main.remove();
+
+var newHeader = document.createElement('h1');
+newHeader.id = 'victory';
+
+newHeader.innerHTML = 'Donell is the champion!';
